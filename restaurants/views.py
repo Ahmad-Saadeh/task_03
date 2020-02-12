3a7b05afd986b44b060031b1a48dc1c76b136f9c
Ahmad-Saadeh <Ahmad-Saadeh@users.noreply.github.com>
2020-02-12 14:22:20 +0200

    task_03

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,10 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    "my_list" : [{
+    "restaurant_name":"New restaurant",
+    "food_type":"New food!!"
+    }]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +17,9 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+    "my_object" : {
+    "restaurant_name":"New restaurant",
+    "food_type":"New food!!"
+    }
     }
     return render(request, 'detail.html', context)
